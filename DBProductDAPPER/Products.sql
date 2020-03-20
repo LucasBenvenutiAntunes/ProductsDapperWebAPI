65f5311b808d1c72a47ee8b136711f780457adb8
Lucas Benvenuti Antunes <45469993+LucasBenvenutiAntunes@users.noreply.github.com>
2020-03-20 17:40:29 -0300

    Add files via upload

---
+++ DBProductDAPPER/Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId] INT            IDENTITY (1, 1) NOT NULL,
+    [Name]      NVARCHAR (MAX) NOT NULL,
+    [Price]     FLOAT (53)     NOT NULL,
+    [Created]   NVARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([ProductId] DESC)
+);
+
